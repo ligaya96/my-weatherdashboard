ef39eede30eee3453a61d08121275dae3dcfccdd
ligaya <ligaya96@gmail.com>
2021-01-29 17:31:12 -0800

    adding files,&starting JS

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){}
+)
+//search button
+
+//search multiple cities
+
+// see current/future weather conditions
+
+// 5 day weather forecast
+
+// search history/ local storage
+
+//API
+
+// 
