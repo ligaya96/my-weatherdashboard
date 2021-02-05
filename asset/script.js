b78bc603fb1e19aa24e42881a657ce3582def751
ligaya <ligaya96@gmail.com>
2021-02-04 18:31:09 -0800

    added to JS

--- asset/script.js
+++ asset/script.js
@@ -1,36 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var APIKEY = "ad8618221b820a5c5d95b144d55511c9";
+  //search button/bar
+  $("#searchBtn").on("click", function () {
+    var weatherValue = $("#search").val("");
+    $("#search").val("");
+    console.log(weatherValue);
+    getApi();
+  });
+  // Jquery API/ search results
+  function getApi(weatherValue) {
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + weatherValue + "&appid" + APIKEY;
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var i = 0; i < data.length; i++) {
 
- // var APPKEY = 
-  var weatherValue = $("#search").val();
- //search button/bar
- $("#searchBtn").on("click", function() {
-   $("#search").val ("")
- });
-// Jquery API/ search results
-function getApi(){
- var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + weatherValue;
-  fetch(requestUrl)
-  .then(function(response) { 
-    return response.json();
+        }
+        weatherForcast(weatherValue);
+        UValue(lat,lon);
+        console.log(weatherValue);
+      })
+  }
 
-   })
-   .then(function (data) {
-     })
-}
-  //searchbutton.on("click", getApi);
+  // see current/future weather conditions
 
-
-
-// see current/future weather conditions
-
-// 5 day weather forecast / html addition
-  function weatherforcast(weatherValue) {
+  // 5 day weather forecast / html addition
+  function weatherForcast(weatherValue) {
+    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + weatherValue + "&appid" + APIKEY;
+    fetch(requestUrl)
+    .then
 
   };
 
- // search history/ local storage
+  // search history/ local storage
   // if (searchistory)
 
 
   // UV index
+  function UValue(lat,lon) {
+
+  }
+
 })
